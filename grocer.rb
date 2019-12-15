29164990d7c8d4f77d438974586f4123444b7d98
Gian Moran <gpmoran_@outlook.com>
2019-12-15 19:32:42 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,19 @@
+require 'pry'
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
-  # Consult README for inputs and outputs
+  result = {}
+  index = 0 
+  while index < collection.count do 
+    item_name = collection[index]
+  if name === item_name[:item] 
+   result = item_name
+   return result 
+  end
+  index +=1
+end
+  
+nil
 end
 
 def consolidate_cart(cart)
@@ -9,12 +21,56 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  new_cart = [ ]
+  index = 0 
+  while index < cart.count do 
+    item_name = cart[index]
+    cart_item = find_item_by_name_in_collection(item_name[:item],new_cart)
+    if cart_item != nil 
+      cart_item[:count] += 1
+    else
+      cart_item = cart[index]
+      cart_item[:count] =  1
+      new_cart << cart_item
+    end
+
+    index += 1
+  
+end
+new_cart
 end
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  index = 0 
+  
+  while index < coupons.count do 
+    item_name = coupons[index]
+ 
+  cart_item = find_item_by_name_in_collection(item_name[:item],cart)
+
+    coupon_item = "#{item_name[:item]} W/COUPON"
+    
+    coupon_to_apply_item = find_item_by_name_in_collection(coupon_item, cart)
+    
+    if cart_item && cart_item[:count] >= item_name[:num]
+      if coupon_to_apply_item
+        coupon_to_apply_item[:count] += item_name[:num]
+        cart_item[:count] -=item_name[:num]
+      else
+        coupon_to_apply_item = coupons[index]
+        cart << coupon_to_apply_item
+        cart_item[:count] -= item_name[:num]
+      end
+       
+      end
+       index += 1
+  end
+
+  cart
+
 end
 
 def apply_clearance(cart)
